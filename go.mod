94500e3b5d8ef13e6cbe0eff9f649b18578d815b
zhaocong <1140253608@qq.com>
2020-03-25 14:18:21 +0800

    log

---
+++ go.mod
@@ -0,0 +1,5 @@
+module goExample
+
+go 1.13
+
+require github.com/zhaocong6/goUtils v1.0.2 // indirect
---
+++ log.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	//初始化增加时间和文件行数
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+	f := NewFile()
+	defer f.Close()
+
+	debugLog := log.New(f, "[Debug]", log.LstdFlags|log.Lshortfile)
+	infoLog := log.New(f, "[Info]", log.LstdFlags|log.Lshortfile)
+
+	defer func() {
+		if err := recover(); err != nil {
+			debugLog.Println(err)
+			infoLog.Println(err)
+		}
+	}()
+
+	panic("测试log")
+}
+
+func NewFile() *os.File {
+	f, err := os.OpenFile("./test.log", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return f
+}
